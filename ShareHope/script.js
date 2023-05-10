24786431ad7c3d08390345bd33470b8f903f6dc4
Pedro-Eugelmi <Pedrogabrielmendy@gmail.com>
2023-05-10 17:42:10 -0300

    nova section

---
+++ ShareHope/script.js
@@ -0,0 +1,12 @@
+/* Scroll reveal */
+ScrollReveal({ reset: true ,   
+    distance:'80px',
+    delay:200,
+    duration:2000});
+    
+    ScrollReveal().reveal('.main', { origin:'top' });
+    ScrollReveal().reveal('#information', { origin:'top' });
+    ScrollReveal().reveal('.skills', { origin:'top' });
+    ScrollReveal().reveal('.portfolio', { origin:'top' });
+    ScrollReveal().reveal('.contact', { origin:'top' });
+    
