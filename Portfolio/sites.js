ee869807802dbca2cfaf8fc694a1472fe45958a8
Pedro-Eugelmi <Pedrogabrielmendy@gmail.com>
2023-05-24 16:23:16 -0300

    adicionando a shareHope No Portfolio

---
+++ Portfolio/sites.js
@@ -0,0 +1,11 @@
+let sites = [
+    {id:1, name:"Urna Eletrônica", desc:'Simulação de um urna eletrônica com personagens de desenho animado (Não completo)', img:"https://github.com/Pedro-Eugelmi/Portfolio/blob/master/Portifolio/Assets/siteimages/urna.png?raw=true", git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Urna%20eletronica', git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Urna%20eletronica', link:'../Urna eletronica/index.html'},
+    {id:2, name:"ShareHope", 
+        desc:'Apresentamos um site inovador que facilita doações para causas sociais e comunidades carentes, promovendo comunicação eficiente entre os usuários. Instituições compartilham atividades, projetos e necessidades, enquanto os usuários conscientizam através de vídeos. Com interface amigável e acessível, simplificamos doações e incentivamos participação ativa em causas sociais. Junte-se a nós nessa jornada de solidariedade e transformação social.',
+        img:'Assets/siteimages/sharehopeimages/modalimage.jpg', git:'https://github.com/Pedro-Eugelmi/Projetos-IF/tree/master/ShareHope', link:'https://pedro-eugelmi.github.io/Projetos-IF/ShareHope/index.html'},
+    {id:3, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
+    {id:4, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
+    {id:5, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
+    {id:6, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
+]
+
--- Portifolio/sites.js
+++
@@ -1,9 +0,0 @@
-let sites = [
-    {id:1, name:"Urna Eletrônica", desc:'Simulação de um urna eletrônica com personagens de desenho animado (Não completo)', img:"Assets/siteimages/urna.png", git:'', link:'../Urna eletronica/index.html'},
-    {id:2, name:"ShareHope", desc:'', img:'Assets/EM-BREVE....png', git:''},
-    {id:3, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
-    {id:4, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
-    {id:5, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
-    {id:6, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
-]
-
