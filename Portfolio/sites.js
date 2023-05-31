eea9268040fb68eaca2f663bb21a8db1e82f8fc5
Pedro-Eugelmi <Pedrogabrielmendy@gmail.com>
2023-05-31 10:11:26 -0300

    add sites

--- Portfolio/sites.js
+++ Portfolio/sites.js
@@ -1,18 +1,39 @@
 let sites = [
-    {id:2, name:"Calculadora de Massa Corporal", desc:'Calculadora de Massa corporal feita com ReactJS e Typescript', 
-    img:'Assets/siteimages/masscalculator/big.jpg', git:''},    
+    {id:1, name:"Calculadora de Massa Corporal", 
+    desc:'Calculadora de Massa corporal feita com ReactJS e Typescript', 
+    img:'Assets/siteimages/masscalculator/big.jpg', 
+    git:'',
+    link:''},    
 
-    {id:2, name:"Urna Eletrônica", desc:'Simulação de um urna eletrônica com personagens de desenho animado (Não completo)', img:"https://github.com/Pedro-Eugelmi/Portfolio/blob/master/Portfolio/Assets/siteimages/urna.png?raw=true", git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Urna%20eletronica', git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Urna%20eletronica', link:'../Urna eletronica/index.html', img:'https://github.com/Pedro-Eugelmi/Portfolio/blob/master/Portfolio/Assets/siteimages/urnaimages/urna.png?raw=true'},
+    {id:2, name:"Urna Eletrônica",
+     desc:'Simulação de um urna eletrônica com personagens de desenho animado (Não completo)', 
+     img:"https://github.com/Pedro-Eugelmi/Portfolio/blob/master/Portfolio/Assets/siteimages/urna.png?raw=true", 
+     git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Urna%20eletronica',
+     link:'../Urna eletronica/index.html', 
+     img:'https://github.com/Pedro-Eugelmi/Portfolio/blob/master/Portfolio/Assets/siteimages/urnaimages/urna.png?raw=true'},
     
     {id:3, name:"Site de venda de Pizzas com Javascript", 
-    desc:'Bem-vindo ao site de pizza da Pizzaria Eugelmi! Aqui você pode escolher suas pizzas favoritas, adicionar ao carrinho, personalizar com coberturas e tamanhos, e finalmente fazer o pedido. Aproveite uma experiência saborosa e conveniente!<br> Feito durante ocurso de Javascript', img:'Assets/EM-BREVE....png', git:''},
+    desc:'Bem-vindo ao site de pizza da Pizzaria Eugelmi! Aqui você pode escolher suas pizzas favoritas, adicionar ao carrinho, personalizar com coberturas e tamanhos, e finalmente fazer o pedido. Aproveite uma experiência saborosa e conveniente!<br> Feito durante ocurso de Javascript', 
+    img:'Assets/siteimages/pizzaria/big.jpg',
+    link:'../pizzaria/index.html',
+     git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Pizzaria'},
     
     {id:4, name:"ShareHope", 
         desc:'Apresentamos um site inovador que facilita doações para causas sociais e comunidades carentes, promovendo comunicação eficiente entre os usuários. Instituições compartilham atividades, projetos e necessidades, enquanto os usuários conscientizam através de vídeos. Com interface amigável e acessível, simplificamos doações e incentivamos participação ativa em causas sociais. Junte-se a nós nessa jornada de solidariedade e transformação social.',
-        img:'Assets/siteimages/sharehopeimages/modalimage.jpg', git:'https://github.com/Pedro-Eugelmi/Projetos-IF/tree/master/ShareHope', link:'https://pedro-eugelmi.github.io/Projetos-IF/presentationSH/index.html'},
+        img:'Assets/siteimages/sharehopeimages/modalimage.jpg', 
+        git:'https://github.com/Pedro-Eugelmi/Projetos-IF/tree/master/ShareHope', 
+        link:'https://pedro-eugelmi.github.io/Projetos-IF/presentationSH/index.html'},
 
-    {id:5, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
+    {id:5, name:"API de Clima", 
+    desc:'Site que utiliza uma API de clima para dizer a temperatura, velocidade do vento, direção do vento e clima.<br> feito com HTML & CSS e Javacript',
+    img:'Assets/siteimages/weather/size.jpg',
+    git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Weather',
+    link:'https://pedro-eugelmi.github.io/Portfolio/Weather/index.html'},
 
-    {id:6, name:"Em Breve", desc:'Aguarde que em breve terá atualizações e novos sites para acessar!', img:'Assets/EM-BREVE....png', git:''},
+    {id:6, name:"Bateria Com composição", 
+    desc:'Este é um projeto de uma bateria desenvolvida durante  meus estudos em JavaScript. A bateria conta com nove sons únicos. <br> Além disso, foi adicionada uma área especial para compor suas próprias músicas. Ao clicar no botão "Tocar".', 
+    img:'Assets/siteimages/bateria/big.jpg', 
+    link:'../bateria/index.html',
+    git:'https://github.com/Pedro-Eugelmi/Portfolio/tree/master/Bateria'},
 ]
 
